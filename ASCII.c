a1e3851ce6cae92c5cac93da759b5fd497da4881
Ayush Chaurasiya <110969828+Ayush365@users.noreply.github.com>
2023-10-08 20:07:39 +0530

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,12 @@
+//print ASCII number
+/* its a simple code , we just need to input a variable as character
+then print it as integer , this will print the ascii number/code of that character,
+for example - try to write/input 'a' in terminal and it will print 97 , which is the ascii code of 'a' */
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    printf("%d",ch);
+    return 0;
+}
